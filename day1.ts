3ae173e25e69d17a235b5627f01976bcb24bf17d
tuyet22 <caothianhtuyet221@gmail.com>
2026-01-08 17:42:11 +0700

    update

--- day1.ts
+++ day1.ts
@@ -1,1 +1,16 @@
 let fullName:string='Tuyet';
+let age:number=22;
+let isMarried:boolean=false;
+//array
+let scores:number []= [9,8,7,6,5];
+// tuple
+let account:[string, number, boolean]=['Tuyet',22,false];
+// union
+let productid:string|number;
+
+productid=12345;
+productid='ABC123';
+// union with initial value
+let productId: string | number = "PRD-1001";
+
+console.log(`Name: ${fullName}, Age: ${age}, Married: ${isMarried}`);
